b22153ba6ed0d9ae2fd55dba77312ac7827141a5
RISHABH SHAH <rnshah1997@gmail.com>
2024-11-19 00:34:26 -0500

    Second update1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
   return (
     <section className="about section" id="about">
       <h2 className="section__title">About Me</h2>
-      <span className="section__subtitle">My Intoduction</span>
+      <span className="section__subtitle">Professional Snapshot</span>
 
       <div className="about__container container grid">
         <img
